30c14b91f51c8d16e66391a76b1176b812dbfaf1
Ochicobrain07 <99326420+Ochicobrain07@users.noreply.github.com>
2022-02-16 13:04:13 +0000

    ghgvhgf

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 function Para_Func() {
     document.getElementById("target").innerHTML = "Hello Jude!"
 }
+
+function Para_third_year() {
+    document.getElementById("third_year").innerHTML = "Hiyeah!"
+}
